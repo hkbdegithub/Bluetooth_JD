a5825039cf2aaa110aed3913f0a35f79cec32c12
。黑火 <940116935@qq.com>
2017-11-03 12:07:11 +0800

    蓝牙程序——结对

---
+++ Bluetooth3.0/Bluetooth2.0Demo/Logo.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Threading;
+
+namespace Bluetooth2._0Demo
+{
+    /// <summary>
+    /// Window1.xaml 的交互逻辑
+    /// </summary>
+    public partial class Window1
+    {
+        private int _totalTime;
+
+        public Window1()
+        {
+            InitializeComponent();
+
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+
+            var timer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(1) };
+            timer.Tick += timer_Tick;
+            timer.Start();
+
+            //sbar.Items.Clear();/////
+
+            //Label lbl = new Label();
+            //lbl.Background = new LinearGradientBrush(Colors.LightBlue, Colors.SlateBlue, 90);
+            //lbl.Content = "ProgressBar with one iteration.";
+            //sbar.Items.Add(lbl);
+            ProgressBar progbar = new ProgressBar
+            {
+                IsIndeterminate = false,
+                Orientation = Orientation.Horizontal,
+                Foreground = Brushes.Blue,
+                Width = 480,
+                Height = 15
+            };
+            Duration duration = new Duration(TimeSpan.FromSeconds(7.5));
+            DoubleAnimation doubleanimation = new DoubleAnimation(100.0, duration);
+            progbar.BeginAnimation(RangeBase.ValueProperty, doubleanimation);
+            //sbar.Items.Add(progbar);/////
+
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            _totalTime++; //时间自增
+            //label.Content = _totalTime.ToString();
+            if (_totalTime == 3)
+            {
+                Close();
+            }
+        }
+    }
+}
---
+++ Bluetooth3.0/Bluetooth2.0Demo/StuInfo.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Bluetooth2._0Demo
+{
+    /// <summary>
+    /// StuInfo.xaml 的交互逻辑
+    /// </summary>
+    public partial class StuInfo
+    {
+
+        private readonly ObservableCollection<Student> _stus;
+
+        public StuInfo(List<string> a)
+        {
+            InitializeComponent();
+
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+
+            _stus = new ObservableCollection<Student>
+            {
+                new Student()
+                {
+                    Id = a[1],
+                    Name = a[0],
+                    Time = a[2],
+                    Mac = a[3],
+                }
+            };
+
+            //MessageBox.Show(a[0]);
+
+            ListBoxShow.ItemsSource = _stus;
+
+
+        }
+
+        private void BT_close(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Grid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void ListBoxShow_SelectionChanged(object sender, MouseButtonEventArgs e)
+        {
+            DatabaseProcessing.DeleteInfo(_stus[0].Mac);
+            Close();
+        }
+    }
+}
